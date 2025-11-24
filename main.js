36d0154c6331a6ba934e030d1f9506eaf134c8a2
Lila <lila.bahuet@gmail.com>
2025-11-24 12:30:20 +0100

    feat: 更新探索音樂的 ABC 記譜，新增和聲、蛇魅笛及鼓聲部，提升音樂層次感

--- main.js
+++ main.js
@@ -9,30 +9,111 @@ const MusicSystem = {
 	
 	// ABC 記譜 - 探索音樂
 	explorationMusic: `
-X:1
-T:Egypt_Stage_32bars
+X:30
+T:Egypt_Stage_Full_with_Pungi_32bars
 M:4/4
 L:1/8
 Q:160
 K:Aphr
-% Lead melody in A Phrygian Dominant – Egyptian vibe
-[V:Lead]
 
-% --- Section A (bars 1–8): Main Theme ---
+V:Lead clef=treble
+V:Harmony clef=treble
+V:Pungi clef=treble
+V:Bass clef=bass
+V:Drums clef=perc
+
+%%score (Lead Harmony Pungi Bass Drums)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% --- LEAD（原曲主旋律） ---
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+V:Lead
+% Section A (1–8)
 A4 C2 D2 | E4 F2 E2 | A4 G2 F2 | E4 C2 B,2 |
 A4 C2 D2 | E4 F2 A2 | G4 F2 E2 | A6 z2 |
-
-% --- Section B (bars 9–16): Bright Desert Motif ---
+% Section B (9–16)
 C'4 B2 A2 | G4 F2 E2 | F4 E2 D2 | C4 B,2 A,2 |
 A4 C2 D2 | E4 F2 E2 | G4 F2 E2 | A6 z2 |
-
-% --- Section C (bars 17–24): Mysterious Pyramid Passage ---
+% Section C (17–24)
 E4 F2 G2 | A4 G2 F2 | C'4 B2 A2 | G4 F2 E2 |
 D4 C2 B,2 | A,4 B,2 C2 | D4 E2 F2 | G6 z2 |
-
-% --- Section D (bars 25–32): Return + Climax ---
+% Section D (25–32)
 A4 C2 D2 | E4 F2 E2 | A4 G2 F2 | E4 C2 B,2 |
 A4 C2 E2 | F4 G2 A2 | G4 F2 E2 | A8 ||
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% --- HARMONY（和聲＋第二旋律） ---
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+V:Harmony
+% Section A
+E2 A2 G2 A2 | F4 E2 D2 | E2 G2 A2 B2 | A4 G2 F2 |
+A2 A2 F2 A2 | G4 F2 E2 | C'4 B2 A2 | G6 z2 |
+% Section B
+A2 G2 F2 E2 | D4 C2 B,2 | E2 G2 C'2 B2 | A4 G2 E2 |
+C'2 B2 A2 G2 | F2 E2 D2 C2 | E2 F2 G2 A2 | C'6 z2 |
+% Section C
+A2 B2 C'2 D'2 | E'4 D'2 C'2 | C'2 B2 A2 G2 | F4 E2 D2 |
+E2 A2 C'2 B2 | A4 G2 F2 | E2 G2 A2 B2 | C'4 B2 A2 |
+% Section D
+E2 A2 G2 A2 | F4 E2 D2 | E2 G2 A2 B2 | A4 G2 F2 |
+A2 C'2 B2 A2 | G2 F2 E2 D2 | F2 E2 D2 C2 | A8 ||
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% --- PUNGI（蛇魅笛） ---
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+V:Pungi
+% Section A (1–8)
+A2 ^B2 C'2 A2 | C'3 D' C'2 A2 | G2 A2 C'2 B2 | A4 G2 F2 |
+A2 C'2 A2 C'2 | D'3 C' B2 A2 | G2 A2 F2 G2 | A8 |
+% Section B (9–16)
+C'4 B2 A2 | G2 A2 C'2 B2 | A4 G2 F2 | E4 F2 G2 |
+A2 C'2 B2 A2 | C'4 D'2 C'2 | B2 C'2 D'2 E'2 | A8 |
+% Section C (17–24)
+E'2 D'2 C'2 B2 | A4 ^G2 A2 | C'2 B2 A2 G2 | F2 G2 A2 F2 |
+E2 A2 C'2 B2 | A3 ^G A2 F2 | G2 A2 B2 C'2 | A8 |
+% Section D (25–32)
+A2 C'2 A2 C'2 | D'3 C' B2 A2 | G2 A2 C'2 B2 | A4 G2 F2 |
+A2 C'2 B2 A2 | G2 F2 E2 D2 | F2 E2 D2 C2 | A8 ||
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% --- BASS（16-bit Saw Bass） ---
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+V:Bass
+% Section A
+[A,2 A2] [A,2 A2] | [D2 D2] [C2 C2] |
+[A,2 A2] [C2 C2] | [D2 D2] [E2 E2] |
+[F2 F2] [E2 E2] | [A,2 A2] [G2 G2] |
+[F2 F2] [E2 E2] | [A,4 A,4] |
+% Section B
+[A,2 A2] [A,2 A2] | [D2 D2] [C2 C2] |
+[A,2 A2] [C2 C2] | [D2 D2] [E2 E2] |
+[F2 F2] [E2 E2] | [A,2 A2] [G2 G2] |
+[F2 F2] [E2 E2] | [A,4 A,4] |
+% Section C
+[A,2 A2] [A,2 A2] | [D2 D2] [C2 C2] |
+[A,2 A2] [C2 C2] | [D2 D2] [E2 E2] |
+[F2 F2] [E2 E2] | [A,2 A2] [G2 G2] |
+[F2 F2] [E2 E2] | [A,4 A,4] |
+% Section D
+[A,2 A2] [A,2 A2] | [D2 D2] [C2 C2] |
+[A,2 A2] [C2 C2] | [D2 D2] [E2 E2] |
+[F2 F2] [E2 E2] | [A,2 A2] [G2 G2] |
+[F2 F2] [E2 E2] | [A,4 A,4] ||
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% --- DRUMS（SNES/Genesis 風節奏） ---
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+V:Drums
+% Bass drum = C, Snare = X, Hi-hat = ^
+[C2 ^z C2 ^z] | [X2 ^z X2 ^z] |
+[C2 ^C ^C C] | [X2 ^z X2 ^z] |
+[C2 ^z C2 ^z] | [X2 ^z X2 ^z] |
+[C2 ^C ^C C] | [X2 ^z X2 ^z] |
+% repeat for all 32 bars
+[C2 ^z C2 ^z] | [X2 ^z X2 ^z] |
+[C2 ^C ^C C] | [X2 ^z X2 ^z] |
+[C2 ^z C2 ^z] | [X2 ^z X2 ^z] |
+[C2 ^C ^C C] | [X2 ^z X2 ^z] ||
 `,
 	
 	// ABC 記譜 - 戰鬥音樂（僅使用主旋律，簡化版本）
